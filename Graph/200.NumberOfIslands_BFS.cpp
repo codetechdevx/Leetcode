b5135f1a9a72eb287cdefb8b17b6b32deba76609
Ankit Anudeep <ankitanudeep@gmail.com>
2025-08-07 23:27:27 +0530

    Leetcode 200

---
+++ Graph/200.NumberOfIslands_BFS.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.size() == 0)
+            return 0;
+        int nr = grid.size();
+        int nc = grid[0].size();
+
+        int num = 0;
+        for(int i = 0; i < nr; i++)
+        {
+            for(int j = 0; j < nc; j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    num++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+
+                    while(!q.empty())
+                    {
+                        auto current = q.front();
+                        q.pop();
+                        int row = current.first;
+                        int col = current.second;
+
+                        if(row-1 >= 0 && grid[row-1][col] == '1')
+                        {
+                            q.push({row-1, col});
+                            grid[row-1][col] = '0';
+                        }
+                        if(row+1 < nr && grid[row+1][col] == '1')
+                        {
+                            q.push({row+1, col});
+                            grid[row+1][col] = '0';
+                        }
+                        if(col-1 >= 0 && grid[row][col-1] == '1')
+                        {
+                            q.push({row, col-1});
+                            grid[row][col-1] = '0';
+                        }
+                        if(col+1 < nc && grid[row][col+1] == '1')
+                        {
+                            q.push({row, col+1});
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }   
+        }
+
+        return num;
+    }
+};
+
+int main() {
+    vector<vector<char>> grid = {
+        {'1','1','0','0','0'},
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'}
+    };
+    Solution sol;
+    int result = sol.numIslands(grid);
+    cout << "Number of islands: " << result << endl;
+    return 0;
+}
+
---
+++ Graph/200.NumberOfIslands_DFS.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int r, int c)
+    {
+        int nr = grid.size();
+        int nc = grid[0].size();
+        grid[r][c] = '0';
+
+        if(r - 1 >= 0 && grid[r-1][c] == '1')   dfs(grid, r-1, c);  
+        if(r + 1 < nr && grid[r+1][c] == '1')   dfs(grid, r+1, c);
+        if(c - 1 >= 0 && grid[r][c-1] == '1')   dfs(grid, r, c-1);
+        if(c + 1 < nc && grid[r][c+1] == '1')   dfs(grid, r, c+1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.size() == 0)
+            return 0;
+        int nr = grid.size();
+        int nc = grid[0].size();
+        int num_islands = 0;
+
+        for(int i = 0; i < nr; i++)
+        {
+            for(int j = 0; j < nc; j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    dfs(grid, i, j);
+                    num_islands++;
+                }
+            }
+        }
+
+        return num_islands;
+    }
+};
+
+int main() {
+    vector<vector<char>> grid = {
+        {'1','1','0','0','0'},
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'}
+    };
+    Solution sol;
+    int result = sol.numIslands(grid);
+    cout << "Number of islands: " << result << endl;
+}
